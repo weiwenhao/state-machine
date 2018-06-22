ad6c492d2805f17d00a6cd9b78fff6f9750cefcc
weiwenhao <1101140857@qq.com>
2018-06-22 11:40:30 +0800

    add graph file command

---
+++ src/Providers/StateMachineServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Weiwenhao\StateMachine\Providers;
+
+use Weiwenhao\StateMachine\Commands\GraphMakeCommand;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                GraphMakeCommand::class
+            ]);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
